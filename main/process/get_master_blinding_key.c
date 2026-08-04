76d26502f323857d967017b90b725e9153e0ca2b
Jon Griffiths <jon_p_griffiths@yahoo.com>
2026-08-04 23:19:58 +1200

    rpc: fix back-compat check for only_if_silent bool

--- main/process/get_master_blinding_key.c
+++ main/process/get_master_blinding_key.c
@@ -24,7 +24,7 @@ void get_master_blinding_key_process(void* process_ptr)
 
         CborValue params;
         const CborError cberr = cbor_value_map_find_value(&process->ctx.value, CBOR_RPC_TAG_PARAMS, &params);
-        if (cberr == CborNoError || cbor_value_is_valid(&params) || cbor_value_is_map(&params)) {
+        if (cberr == CborNoError && cbor_value_is_valid(&params) && cbor_value_is_map(&params)) {
             // This field is optional and defaults to false if not present (initialized above)
             only_if_silent = rpc_get_bool_or("only_if_silent", &params, false);
         }
